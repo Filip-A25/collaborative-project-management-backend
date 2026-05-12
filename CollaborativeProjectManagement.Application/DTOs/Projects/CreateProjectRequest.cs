22d879f6c1d1235202006cbc1c89ffa13d28d4e3
Filip-A25 <filip.agatic25@gmail.com>
2026-05-12 20:55:20 +0200

    Add required attributes to request dtos instead of modifiers

--- CollaborativeProjectManagement.Application/DTOs/Projects/CreateProjectRequest.cs
+++ CollaborativeProjectManagement.Application/DTOs/Projects/CreateProjectRequest.cs
@@ -6,11 +6,15 @@ namespace CollaborativeProjectManagement.Application.DTOs.Projects
     public class CreateProjectRequest
     {
         [MaxLength(120)]
-        public required string Name { get; set; }
+        [Required]
+        public string Name { get; set; }
         public string? Description { get; set; }
+        [Required]
         public required DateOnly StartDate { get; set; }
+        [Required]
         public required DateOnly EndDate { get; set; }
         [MaxLength(3)]
+        [Required]
         public required string Currency { get; set; }
         public double? BudgetAmount { get; set; }
         public List<ProjectRole>? Roles { get; set; }
