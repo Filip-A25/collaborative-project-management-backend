3a31ceb127c5af3024280a5407d4670efe1edda1
Filip-A25 <filip.agatic25@gmail.com>
2026-08-29 14:32:17 +0200

    Update response messages

--- CollaborativeProjectManagement.Application/Common/ResponseMessage.cs
+++ CollaborativeProjectManagement.Application/Common/ResponseMessage.cs
@@ -12,14 +12,14 @@
             // Error messages
             public const string RegisterConflict = "An account with this email already exists.";
             public const string UserNotFound = "User could not be found.";
-            public const string IncorrectPassword = "Incorrect password entered.";
+            public const string IncorrectPassword = "Username or password is incorrect.";
             public const string NonUniqueUsername = "This username is already taken.";
             public const string NonUniqueEmail = "This email is already taken.";
 
             // Success messages
-            public const string RegisterSuccess = "User has been successfully registered.";
-            public const string LoginSuccess = "User has successfully logged in.";
-            public const string UpdateSuccess = "User has been successfully updated.";
+            public const string RegisterSuccess = "You have successfully registered.";
+            public const string LoginSuccess = "You have successfully logged in.";
+            public const string UpdateSuccess = "You have successfully updated user information.";
         }
 
         public static class Projects
@@ -28,15 +28,15 @@
             public const string AuthorizationError = "User has to be an Admin to create a project.";
             public const string ProjectNotFound = "Project could not be found.";
             public const string ProjectsDontExist = "User does not have any projects.";
-            public const string UserNotMember = "User is not a member of this project.";
+            public const string UserNotMember = "You are not a member of this project.";
             public const string MemberNotFound = "Member could not be found.";
             public const string CreatorRemoveFail = "Creator cannot be removed from the project.";
     
             // Unauthorized role errors
-            public const string ProjectRoleDeleteError = "User does not have sufficient permissions to delete the project.";
-            public const string ProjectRoleViewError = "User does not have sufficient permissions to view the project.";
-            public const string ProjectManageError = "User does not have sufficient permissions to manage this project.";
-            public const string ProjectMembersRemoveError = "User does not have sufficient permissions to remove members from this project.";
+            public const string ProjectRoleDeleteError = "You do not have sufficient permissions to delete the project.";
+            public const string ProjectRoleViewError = "You do not have sufficient permissions to view the project.";
+            public const string ProjectManageError = "You do not have sufficient permissions to manage this project.";
+            public const string ProjectMembersRemoveError = "You do not have sufficient permissions to remove members from this project.";
 
             // Success messages
             public const string CreateSuccess = "Project has been successfully created.";
@@ -51,7 +51,7 @@
             public const string RoleNotFound = "Role could not be found.";
             public const string ProjectIdMissing = "Project ID needs to be provided to create a role.";
             // Unauthorized role errors
-            public const string RolesManageError = "User does not have sufficient permissions to manage project roles.";
+            public const string RolesManageError = "You do not have sufficient permissions to manage project roles.";
 
             // Success messages
             public const string AddPermissionsSuccess = "Successfully added permissions to the role.";
@@ -66,9 +66,9 @@
             public const string InviteNotFound = "Invite not found.";
 
             // Unauthorized invites errors
-            public const string InvitesCreateError = "User does not have sufficient permissions to create invites.";
-            public const string InvitesDeleteError = "User does not have sufficient permissions to delete invites.";
-            public const string InvitesFetchError = "User does not have sufficient permissions to fetch project invites.";
+            public const string InvitesCreateError = "You do not have sufficient permissions to create invites.";
+            public const string InvitesDeleteError = "You do not have sufficient permissions to delete invites.";
+            public const string InvitesFetchError = "You do not have sufficient permissions to fetch project invites.";
             public const string InvitesUserAlreadyMemberError = "User is already a member of the project.";
 
             // Success messages
@@ -80,13 +80,13 @@
         public static class Tasks
         {
             // Error messages
-            public const string TasksManageError = "User does not have sufficient permissions to manage tasks.";
+            public const string TasksManageError = "You do not have sufficient permissions to manage tasks.";
             public const string ProjectTasksNotFound = "No tasks were found for the project.";
             public const string MemberNotInProject = "Assigned user is not a member in this project.";
             public const string TaskNotFound = "Task not found.";
 
             // Unauthorized tasks errros
-            public const string ViewTasksError = "User does not have sufficient permissions to view tasks in this project.";
+            public const string ViewTasksError = "You do not have sufficient permissions to view tasks in this project.";
 
             // Success messages
             public const string CreateSuccess = "Task has been successfully created.";
@@ -108,11 +108,11 @@
         public static class TaskComments
         {
             // Error messages
-            public const string NoPermission = "User does not have permission to edit this comment.";
+            public const string NoPermission = "You do not have permission to edit this comment.";
             public const string BatchNotFound = "No task comments found.";
 
             // Unauthorized task comments errors
-            public const string TaskCommentsViewError = "User does not have sufficient permissions to view tasks in this project.";
+            public const string TaskCommentsViewError = "You do not have sufficient permissions to view tasks in this project.";
 
             // Success messages
             public const string CreateSuccess = "Task comment has been successfully created.";
